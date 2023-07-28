93b29a0b50a30c99922b71d3b9ec49ef6ab4e13f
Joseph Masone <joemasone050897@gmail.com>
2023-07-28 00:46:38 -0400

    change orientation of display for pokemon info

--- src/AttributeList.tsx
+++ src/AttributeList.tsx
@@ -12,25 +12,29 @@ export const AttribueList = (props: Pokemon) => {
     <div className='attributeList'>
       <img src={props.image}></img>
       <div className='attribute'>
+        <label htmlFor='Type'>Gen.</label>
+        <br />
         <input
           type='text'
           id='generation'
           readOnly={true}
           placeholder={props.generation}
         />
-        <label htmlFor='Type'>Gen.</label>
       </div>
       <div className='attribute'>
-        <input type='text' id='ID' readOnly={true} placeholder={props.id} />
         <label htmlFor='ID'>ID</label>
+        <br />
+        <input type='text' id='ID' readOnly={true} placeholder={props.id} />
       </div>
       <div className='attribute'>
-        <input type='text' id='Name' readOnly={true} placeholder={props.name} />
         <label htmlFor='Name'>Name</label>
+        <br />
+        <input type='text' id='Name' readOnly={true} placeholder={props.name} />
       </div>
       <div className='attribute'>
-        <input type='text' id='Type' readOnly={true} placeholder={props.type} />
         <label htmlFor='Type'>Type</label>
+        <br />
+        <input type='text' id='Type' readOnly={true} placeholder={props.type} />
       </div>
       <p>{props.description}</p>
     </div>
